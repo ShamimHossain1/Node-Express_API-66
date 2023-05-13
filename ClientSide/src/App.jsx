5826fbf10516d66ee4711639da06339d665b5ea8
ShamimHossain1 <beastb697@gmail.com>
2023-05-13 22:41:51 +0600

    send req from client side to server side

--- ClientSide/src/App.jsx
+++ ClientSide/src/App.jsx
@@ -1,16 +1,67 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from 'flowbite-react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setCount(data))
+  }, [])
 
+
+
+  const handleAddUser = event => {
+    // console.log('lol')
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+    console.log(user)
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+
+
+
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        const newUsers = [...count, data]
+        setCount(newUsers)
+        form.reset();
+      })
+
+    console.log(count)
+
+  }
   return (
-    <div className="App">
-       <h1 className="text-3xl text-center mt-44 font-bold underline">
-      Hello world!
-    </h1>
+    <div className="text-3xl text-center mt-44 font-bold">
+      <h1 >
+        Hello world
+        {count.map(user =>
+          <p key={user.id}>{user.id} : {user.name} : {user.email}</p>
+
+        )}
+
+      </h1>
+
+      <form onSubmit={handleAddUser} >
+        <input type="text" name='name' /><br />
+        <input type="email" name='email' /><br />
+        <Button className='mx-auto'><input type="submit" value="Submit" /></Button>
+
+      </form>
+
+
     </div>
   )
 }
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+app.use(cors());
+app.use(express.json());
 const users = [
     { id: 1, name: 'shamim', email: 'shamim@gamil.com' },
     { id: 2, name: 'mim', email: 'mim@gamil.com' },
@@ -15,6 +17,17 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res)=>{
     res.send(users);
+})
+
+app.post('/users', (req, res) =>{
+    console.log('post api hitting')
+    console.log(req.body)
+    const newUser = req.body;
+    const id = users.length + 1;
+    newUser.id = id;
+    users.push(newUser);
+    res.send(newUser);
+
 })
 
 app.listen(port, () => {
